15e1158a84dec5b325b0c2561211d31d53c1e2a4
Marcus Balbi <marcus.balbi@a4pm.com.br>
2021-02-12 22:36:23 -0300

    testing video

--- encoder/domain/video.go
+++ encoder/domain/video.go
@@ -1,10 +1,32 @@
 package domain
 
-import "time"
+import (
+	"github.com/asaskevich/govalidator"
+	"time"
+)
 
 type Video struct {
-	ID         string
-	ResourceID string
-	FilePath   string
-	CreatedAt  time.Time
+	ID         string    `valid:"uuid"`
+	ResourceID string    `valid:"notnull"`
+	FilePath   string    `valid:"notnull"`
+	CreatedAt  time.Time `valid:"~"`
+}
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+// NewVideo cria um novo Video
+func NewVideo() *Video {
+	return &Video{}
+}
+
+// Validate valida se existe algum erro no objeto
+func (v *Video) Validate() error {
+	_, err := govalidator.ValidateStruct(v)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
---
+++ encoder/domain/video_test.go
@@ -0,0 +1,41 @@
+package domain_test
+
+import (
+	"encoder/domain"
+	"testing"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateIfVideoIsEmpty(t *testing.T) {
+	video := domain.NewVideo()
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoIdIsNotAUUID(t *testing.T) {
+	video := domain.NewVideo()
+	video.ID = "abc"
+	video.ResourceID = "aa"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoValidation(t *testing.T) {
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.ResourceID = "aa"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+
+	require.Nil(t, err)
+}
